72d7fa83b91c99aaadf625e2b72f43c8e01f8d30
htmahl <matheus.domingues126@gmail.com>
2024-11-14 22:03:31 -0300

    Consumir JSON com dados de receitas e despesas

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,18 +1,68 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, FlatList } from "react-native";
 import React from "react";
+import data from "@/data/bd.json"
 
 export default function Page() {
-    return(
+    return (
         <View style={styles.container}>
-            <Text>Low Profile!</Text>
+            <Text style={styles.header}>Receitas</Text>
+            <FlatList
+                style={styles.list}
+                data={data.receitas}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.item}>
+                        <Text style={styles.descricao}>{item.descricao}</Text>
+                        <Text style={styles.valor}>R$ {item.valor.toFixed(2)}</Text>
+                    </View>
+                )}
+            />
+
+            <Text style={styles.header}>Despesas</Text>
+            <FlatList
+                style={styles.list}
+                data={data.despesas}
+                keyExtractor={(item) => item.id.toString()}
+                renderItem={({ item }) => (
+                    <View style={styles.item}>
+                        <Text style={styles.descricao}>{item.descricao}</Text>
+                        <Text style={styles.valor2}>R$ {item.valor.toFixed(2)}</Text>
+                    </View>
+                )}
+            />
         </View>
+
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        padding: 20,
+    },
+    header: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 10,
+        marginTop: 30,
+    },
+    list:{
+        flex: 1,
+    },
+    item: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 8,
+    },
+    descricao: {
+        fontSize: 18,
+    },
+    valor: {
+        fontSize: 18,
+        color: 'green',
+    },
+    valor2: {
+        fontSize: 18,
+        color: 'red',
     },
 });
